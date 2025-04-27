6fd0b2154ac3a8b7c27023f41eb37818f72fc129
yukisedhu <yukisedhu.r@heptagon.in>
2025-04-27 17:13:39 +0530

    nav bar and top bar color fix

--- frontend/adhash-ecommerce-fe/src/app/components/shared/notification/notification.component.ts
+++ frontend/adhash-ecommerce-fe/src/app/components/shared/notification/notification.component.ts
@@ -1,7 +1,7 @@
-// src/app/shared/components/notification/notification.component.ts
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NotificationService } from '../../../services/notification.service'
+import { NotificationService } from '../../../services/notification.service';
+import { Subscription, timer } from 'rxjs';
 
 @Component({
   selector: 'app-notification',
@@ -17,10 +17,33 @@ import { NotificationService } from '../../../services/notification.service'
   `,
   styleUrls: ['./notification.component.scss']
 })
-export class NotificationComponent {
-  constructor(public notificationService: NotificationService) {}
+export class NotificationComponent implements OnDestroy {
+  private autoCloseSub?: Subscription;
 
+  constructor(public notificationService: NotificationService) {
+    this.notificationService.notification$.subscribe((message) => {
+      if (message) {
+        // Start/reset the auto-close timer
+        this.startAutoCloseTimer();
+      }
+    });
+  }
+
+  startAutoCloseTimer() {
+    this.autoCloseSub?.unsubscribe();
+    // Start a 4-second timer
+    this.autoCloseSub = timer(4000).subscribe(() => {
+      this.notificationService.hide();
+    });
+  }
+
+  // Manual close and cancel timer
   close() {
+    this.autoCloseSub?.unsubscribe();
     this.notificationService.hide();
+  }
+
+  ngOnDestroy() {
+    this.autoCloseSub?.unsubscribe();
   }
 }
